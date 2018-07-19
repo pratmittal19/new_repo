0466082cb8668d83b3e8776665260a182887b218
Tech <pratibha.mittal@voylla.com>
2018-07-19 12:41:08 +0530

    mail

--- myapp/app/mailers/user_mailer.rb
+++ myapp/app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 	def send_mail_confirmation(user)
 		@user = user
-	    mail(:from => 'pratmittal19@gmail.com', :to => "#{user.email}>", :subject => "Registration Confirmation") do |format|
+	    mail(:from => 'abc@gmail.com', :to => "#{user.email}>", :subject => "Registration Confirmation") do |format|
           format.html { render 'user_mailer/user_login'}
       end
 	end
