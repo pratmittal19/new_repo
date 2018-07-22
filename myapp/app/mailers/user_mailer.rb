d1afa281668bac754dc2c387c4b6620298d93f03
pratmittal19 <pratmittal19@gmail.com>
2018-07-22 07:48:52 -0400

    changes in mailer

--- myapp/app/mailers/user_mailer.rb
+++ myapp/app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 	def send_mail_confirmation(user,url)
 		@user = user
 		@url = url
-	    mail(:from => 'abc@gmail.com', :to => "#{user.email}>", :subject => "Registration Confirmation") do |format|
+	    mail(:from => 'sudotesting123@gmail.com', :to => "#{user.email}>", :subject => "Registration Confirmation") do |format|
           format.html { render 'user_mailer/user_login'}
       end
 	end
